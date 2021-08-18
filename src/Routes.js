b6d442469806e995431864d496dcf85fda80a96b
ㅇㅅ <es@iuseong-ui-MacBookPro.local>
2021-08-18 16:26:27 +0900

    ADD: 초기설정 피드백 반영 완료.

---
+++ src/Routes.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Euiseon's 컴포넌트
+import LoginEuiseon from "./pages/euiseon/Login/Login";
+import MainEuiseon from "./pages/euiseon/Main/Main";
+
+// Jiyoung's 컴포넌트
+import LoginJiyoung from "./pages/jiyoung/Login/Login";
+import MainJiyoung from "./pages/jiyoung/Main/Main";
+
+// sejun's 컴포넌트
+import LoginSejun from "./pages/sejun/Login/Login";
+import MainSejun from "./pages/sejun/Main/Main";
+
+//youngho's 컴포넌트
+import LoginYoungho from "./pages/youngho/Login/Login";
+import MainYoungho from "./pages/youngho/Main/Main";
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/login-euiseon" component={LoginEuiseon} />
+          <Route exact path="/main-euiseon" component={MainEuiseon} />
+
+          <Route exact path="/login-jiyoung" component={LoginJiyoung} />
+          <Route exact path="/main-jiyoung" component={MainJiyoung} />
+
+          <Route exact path="/login-sejun" component={LoginSejun} />
+          <Route exact path="/Main-sejun" component={MainSejun} />
+
+          <Route exact path="/login-youngho" component={LoginYoungho} />
+          <Route exact path="/Main-youngho" component={MainYoungho} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
