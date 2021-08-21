2b88ead843507eaf3d8bce0beb83e5488f5fed92
김세준 <jun@gimsejun-ui-MacBookAir.local>
2021-08-21 18:54:59 +0900

    Nav 컴포넌트 Error 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,7 +9,7 @@ class Nav extends React.Component {
         <div className="navBar">
           <div className="logo">
             <img src={logoImg} alt="" />
-            <sapn className="vertical"></sapn>
+            <span className="vertical"></span>
             <span>Westagram</span>
           </div>
           <div className="searchBar">
@@ -21,7 +21,7 @@ class Nav extends React.Component {
             <a href="/index">
               <svg
                 aria-label="홈"
-                class="_8-yf5 "
+                className="_8-yf5 "
                 fill="#262626"
                 height="22"
                 role="img"
@@ -35,7 +35,7 @@ class Nav extends React.Component {
             <a href="/index">
               <svg
                 aria-label="Direct"
-                class="_8-yf5 "
+                className="_8-yf5 "
                 fill="#262626"
                 height="22"
                 role="img"
@@ -57,9 +57,9 @@ class Nav extends React.Component {
                 width="22"
               >
                 <path
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                   d="M24 0C10.8 0 0 10.8 0 24s10.8 24 24 24 24-10.8 24-24S37.2 0 24 0zm0 45C12.4 45 3 35.6 3 24S12.4 3 24 3s21 9.4 21 21-9.4 21-21 21zm10.2-33.2l-14.8 7c-.3.1-.6.4-.7.7l-7 14.8c-.3.6-.2 1.3.3 1.7.3.3.7.4 1.1.4.2 0 .4 0 .6-.1l14.8-7c.3-.1.6-.4.7-.7l7-14.8c.3-.6.2-1.3-.3-1.7-.4-.5-1.1-.6-1.7-.3zm-7.4 15l-5.5-5.5 10.5-5-5 10.5z"
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                 ></path>
               </svg>
             </a>
